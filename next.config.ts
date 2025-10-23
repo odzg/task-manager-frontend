1746cb45ff31dcd88e1a481b1560f67fbbdf3978
Oded Zilbershlag <123odzil@gmail.com>
2025-10-23 18:06:28 +0300

    fix(next.js): remove deprecated `experimental.reactCompiler` config setting

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,4 @@ import type { NextConfig } from 'next';
 // Validate environment variables during build time
 import './src/env.ts';
 
-export default {
-  experimental: {
-    reactCompiler: true,
-  },
-} satisfies NextConfig;
+export default {} satisfies NextConfig;
